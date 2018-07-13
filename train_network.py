937219cb7efbf86d54b7ff6a934110be03c25d2e
sshah98 <shahsuraj261@gmail.com>
2018-07-13 11:22:36 -0400

    working on classifying with 256

--- train_network.py
+++ train_network.py
@@ -27,14 +27,14 @@ import os
 IMAGE_LABEL = 'santa'
 
 # construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True,
-                help="path to input dataset")
-ap.add_argument("-m", "--model", required=True,
-                help="path to output model")
-ap.add_argument("-p", "--plot", type=str, default="{0}_plot.png".format(IMAGE_LABEL),
-                help="path to output loss/accuracy plot")
-args = vars(ap.parse_args())
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-d", "--dataset", required=True,
+#                 help="path to input dataset")
+# ap.add_argument("-m", "--model", required=True,
+#                 help="path to output model")
+# ap.add_argument("-p", "--plot", type=str, default="{0}_plot.png".format(IMAGE_LABEL),
+#                 help="path to output loss/accuracy plot")
+# args = vars(ap.parse_args())
 
 # initialize the number of epochs to train for, initia learning rate,
 # and batch size
@@ -42,76 +42,103 @@ EPOCHS = 25
 INIT_LR = 1e-3
 BS = 32
 
+master_data = []
+master_labels = []
+
+
 # initialize the data and labels
 print("[INFO] loading images...")
 data = []
 labels = []
 
 # grab the image paths and randomly shuffle them
-imagePaths = sorted(list(paths.list_images(args["dataset"])))
+imagePaths = sorted(list(paths.list_images('256_ObjectCategories/002.american-flag')))
 random.seed(42)
 random.shuffle(imagePaths)
 
-# loop over the input images
 for imagePath in imagePaths:
-    # load the image, pre-process it, and store it in the data list
+    
     image = cv2.imread(imagePath)
     image = cv2.resize(image, (28, 28))
     image = img_to_array(image)
     data.append(image)
-
-    # extract the class label from the image path and update the labels list
+    
     label = imagePath.split(os.path.sep)[-2]
-    label = 1 if label == IMAGE_LABEL else 0
+    print(label)
+    if label == '002.american-flag':
+        label = 2 # take first part of path, convert from str to int
+
     labels.append(label)
 
-# scale the raw pixel intensities to the range [0, 1]
-data = np.array(data, dtype="float") / 255.0
-labels = np.array(labels)
+print(len(labels))
+    
 
-# partition the data into training and testing splits using 75% of
-# the data for training and the remaining 25% for testing
-(trainX, testX, trainY, testY) = train_test_split(
-    data, labels, test_size=0.25, random_state=42)
+# loop over the input images
+# for imagePath in imagePaths:
+# 
+#     print(imagePath)
+#     # load the image, pre-process it, and store it in the data list
+#     image = cv2.imread(imagePath)
+#     image = cv2.resize(image, (28, 28))
+#     image = img_to_array(image)
+#     data.append(image)
+# 
+#     # extract the class label from the image path and update the labels list
+#     label = imagePath.split(os.path.sep)[-2]
+#     label = 1 if label == 'santa' else 0
+#     labels.append(label)
+# 
+# # scale the raw pixel intensities to the range [0, 1]
+# data = np.array(data, dtype="float") / 255.0
+# labels = np.array(labels)
 
-# convert the labels from integers to vectors
-trainY = to_categorical(trainY, num_classes=2)
-testY = to_categorical(testY, num_classes=2)
 
-# construct the image generator for data augmentation
-aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
-                         height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
-                         horizontal_flip=True, fill_mode="nearest")
 
-# initialize the model
-print("[INFO] compiling model...")
-model = LeNet.build(width=28, height=28, depth=3, classes=2)
-opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-model.compile(loss="binary_crossentropy", optimizer=opt,
-              metrics=["accuracy"])
 
-# train the network
-print("[INFO] training network...")
-H = model.fit_generator(aug.flow(trainX, trainY, batch_size=BS),
-                        validation_data=(testX, testY), steps_per_epoch=len(
-                            trainX) // BS,
-                        epochs=EPOCHS, verbose=1)
-
-# save the model to disk
-print("[INFO] serializing network...")
-model.save(args["model"])
-
-# plot the training loss and accuracy
-plt.style.use("ggplot")
-plt.figure()
-N = EPOCHS
-plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
-plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
-plt.title(
-    "Training Loss and Accuracy on {0}/NOT {0}".format(IMAGE_LABEL.upper()))
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend(loc="lower left")
-plt.savefig(args["plot"])
+# 
+# # partition the data into training and testing splits using 75% of
+# # the data for training and the remaining 25% for testing
+# (trainX, testX, trainY, testY) = train_test_split(
+#     data, labels, test_size=0.25, random_state=42)
+# 
+# # convert the labels from integers to vectors
+# trainY = to_categorical(trainY, num_classes=2)
+# testY = to_categorical(testY, num_classes=2)
+# 
+# # construct the image generator for data augmentation
+# aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
+#                          height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
+#                          horizontal_flip=True, fill_mode="nearest")
+# 
+# # initialize the model
+# print("[INFO] compiling model...")
+# model = LeNet.build(width=28, height=28, depth=3, classes=2)
+# opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+# model.compile(loss="binary_crossentropy", optimizer=opt,
+#               metrics=["accuracy"])
+# 
+# # train the network
+# print("[INFO] training network...")
+# H = model.fit_generator(aug.flow(trainX, trainY, batch_size=BS),
+#                         validation_data=(testX, testY), steps_per_epoch=len(
+#                             trainX) // BS,
+#                         epochs=EPOCHS, verbose=1)
+# 
+# # save the model to disk
+# print("[INFO] serializing network...")
+# model.save(args["model"])
+# 
+# # plot the training loss and accuracy
+# plt.style.use("ggplot")
+# plt.figure()
+# N = EPOCHS
+# plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+# plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+# plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
+# plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
+# plt.title(
+#     "Training Loss and Accuracy on {0}/NOT {0}".format(IMAGE_LABEL.upper()))
+# plt.xlabel("Epoch #")
+# plt.ylabel("Loss/Accuracy")
+# plt.legend(loc="lower left")
+# plt.savefig(args["plot"])
