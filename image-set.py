b610e556b8a458f9cdf9044fbe97c06e716ca699
sshah98 <shahsuraj261@gmail.com>
2018-07-13 10:13:13 -0400

    working on getting random images from directory

--- image-set.py
+++ image-set.py
@@ -1,5 +1,5 @@
-# import os
-# 
+import os
+import random
 
 path = '256_ObjectCategories'
 
@@ -7,25 +7,25 @@ path = '256_ObjectCategories'
 # for root, dirs, files in os.walk(path, topdown=False):
 #     for name in dirs:
 #         categories.append(name.split('.')[-1])
-# 
-# 
+#
+#
 # print("Your list is: ", categories)
-# 
+#
 # name = input("Pick category: ")
-# 
+#
 # if any(name in s for s in categories):
 #     print(list(filter(lambda x: name in x, categories)))
 # else:
 #     print("No category of {0}".format(name))
-# 
-# 
-# 
-# 
-import os, random
-n=0
-random.seed();
+#
+
+test = []
+n = 0
+random.seed()
 for root, dirs, files in os.walk(path):
-  for name in files:
-    n=n+1
-    if random.uniform(0, n) < 1: rfile=os.path.join(root, name)
+    for name in files:
+        n = n + 1
+        if random.uniform(0, 100) < 1:
+            rfile = os.path.join(root, name)
+
 print(rfile)
