8b920b9a95638f917d1454385dd92d6fda2bced3
sshah98 <shahsuraj261@gmail.com>
2018-07-16 13:59:50 -0400

    added changes to dev

--- test_network.py
+++ test_network.py
@@ -13,8 +13,6 @@ import sys
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-m", "--model", required=True,
-	help="path to trained model")
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image")
 args = vars(ap.parse_args())
@@ -31,21 +29,29 @@ image = np.expand_dims(image, axis=0)
 
 # load the trained convolutional neural network
 print("[INFO] loading network...")
-model = load_model(args["model"])
+model = load_model('example_model')
+
+
+print(model.predict(image)[0])
+
+
+# print(len(model.predict(image)))
 
 # classify the input image
-(notSanta, santa) = model.predict(image)[0]
 
-# build the label
-label = "Santa" if santa > notSanta else "Not Santa"
-proba = santa if santa > notSanta else notSanta
-label = "{}: {:.2f}%".format(label, proba * 100)
 
-# draw the label on the image
-output = imutils.resize(orig, width=400)
-cv2.putText(output, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX,
-	0.7, (0, 255, 0), 2)
+# (notSanta, santa) = model.predict(image)[0]
 
-# show the output image
-cv2.imshow("Output", output)
-cv2.waitKey(0)
+# # build the label
+# label = "Santa" if santa > notSanta else "Not Santa"
+# proba = santa if santa > notSanta else notSanta
+# label = "{}: {:.2f}%".format(label, proba * 100)
+
+# # draw the label on the image
+# output = imutils.resize(orig, width=400)
+# cv2.putText(output, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX,
+# 	0.7, (0, 255, 0), 2)
+
+# # show the output image
+# cv2.imshow("Output", output)
+# cv2.waitKey(0)
